132568bfe175423d7f613d68862ffa0b7953c07e
Jakerkane <furkan_akman_41@hotmail.com>
2022-03-12 16:14:24 +0300

    Initial Setup

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts")
 const mongoose = require('mongoose')
 
-const indexRouter = require('./routes/index')
-app.use('/',indexRouter)
-
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
 const db = mongoose.connection
 db.on('error',error => console.error(error))
@@ -21,5 +18,7 @@ app.set('layout',"layouts/layout")
 app.use(expressLayouts)
 app.use(express.static('public'))
 
+const indexRouter = require('./routes/index')
+app.use('/',indexRouter)
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT)
