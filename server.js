d052917434a9d3fc53efb6ed498f7c923b10160e
Jakerkane <83370026+Jakerkane@users.noreply.github.com>
2022-03-12 17:11:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts")
 const mongoose = require('mongoose')
 
-//mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
-//const db = mongoose.connection
-//db.on('error',error => console.error(error))
-//db.once('open',() => console.log('connected to mongose'))
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
+const db = mongoose.connection
+db.on('error',error => console.error(error))
+db.once('open',() => console.log('connected to mongose'))
 
 app.set("view engine","ejs")
 app.set('views',__dirname + "/views")
